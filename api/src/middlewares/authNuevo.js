5afc632fbf7549849e07c34190fd551ccd015ef9
Martin Horodeski <martinhorodeski@gmail.com>
2022-11-10 22:08:04 -0300

    Update authNuevo.js

--- api/src/middlewares/authNuevo.js
+++ api/src/middlewares/authNuevo.js
@@ -157,7 +157,7 @@ exports.loginGoogle = async (req, res) => {
     };
     console.log('SOY TOKEN!!', token)
     res.cookie("jwt", token, cookiesOptions); 
-    res.redirect("https://barberspf.vercel.app/");
+    return res.redirect("https://barberspf.vercel.app/");
   } else {
     res.redirect("https://barberspf.vercel.app/login");
   }
